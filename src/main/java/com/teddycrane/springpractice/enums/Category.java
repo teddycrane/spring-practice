240181cb948953ee013b26ef0509a4b4515c8257
Teddy Crane <teddy.crane@slalom.com>
2021-11-03 11:09:13 -0700

    refactor and create local mysql support with docker

--- src/main/java/com/teddycrane/springpractice/enums/Category.java
+++
@@ -1,5 +0,0 @@
-package com.teddycrane.springpractice.enums;
-
-public enum Category {
-	CAT_1, CAT_2, CAT_3, CAT_4, CAT_5
-}
--- startup/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Environment Variables to be loaded here
-SSH_IP=$1
