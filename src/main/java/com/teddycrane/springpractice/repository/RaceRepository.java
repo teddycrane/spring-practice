94428283a080caf43f9e452720aba452e2e3c65f
Teddy Crane <12413402+LTCrane4@users.noreply.github.com>
2021-11-06 07:50:43 -0700

    jdk change to java 17 (only one on apt-get)

--- src/main/java/com/teddycrane/springpractice/repository/RaceRepository.java
+++ src/main/java/com/teddycrane/springpractice/repository/RaceRepository.java
@@ -1,12 +1,9 @@
 package com.teddycrane.springpractice.repository;
 
 import com.teddycrane.springpractice.entity.Race;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
