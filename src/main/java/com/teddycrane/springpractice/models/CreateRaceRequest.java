39835291685072d7342a0cdea508565f823481ae
Teddy Crane <teddy.crane@slalom.com>
2021-11-23 14:17:35 -0800

    update CreateRaceRequest to include optional start date and end date params
    Signed-off-by: Teddy Crane <teddy.crane@slalom.com>

--- src/main/java/com/teddycrane/springpractice/models/CreateRaceRequest.java
+++ src/main/java/com/teddycrane/springpractice/models/CreateRaceRequest.java
@@ -1,8 +1,11 @@
 package com.teddycrane.springpractice.models;
 
+import com.sun.istack.Nullable;
 import com.teddycrane.springpractice.enums.Category;
 
 import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.Optional;
 
 public class CreateRaceRequest
 {
@@ -12,10 +15,31 @@ public class CreateRaceRequest
 	@NotNull
 	private final Category category;
 
+	@Nullable
+	private final Date startTime, endTime;
+
 	public CreateRaceRequest(String name, Category category)
 	{
 		this.name = name;
 		this.category = category;
+		this.startTime = null;
+		this.endTime = null;
+	}
+
+	public CreateRaceRequest(String name, Category category, Date startTime)
+	{
+		this.name = name;
+		this.category = category;
+		this.startTime = new Date(startTime.getTime());
+		this.endTime = null;
+	}
+
+	public CreateRaceRequest(String name, Category category, Date startTime, Date endTime)
+	{
+		this.name = name;
+		this.category = category;
+		this.startTime = new Date(startTime.getTime());
+		this.endTime = new Date(endTime.getTime());
 	}
 
 	public Category getCategory()
@@ -26,5 +50,15 @@ public class CreateRaceRequest
 	public String getName()
 	{
 		return name;
+	}
+
+	public Optional<Date> getStartTime()
+	{
+		return Optional.ofNullable(startTime);
+	}
+
+	public Optional<Date> getEndTime()
+	{
+		return Optional.ofNullable(endTime);
 	}
 }
