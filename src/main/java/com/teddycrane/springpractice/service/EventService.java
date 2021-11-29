5fd579d4faa94f882e2bd1a8ed289bb1ec92c16c
Teddy Crane <teddy.crane@slalom.com>
2021-11-29 14:56:22 -0800

    make eventService public
    Signed-off-by: Teddy Crane <teddy.crane@slalom.com>

--- src/main/java/com/teddycrane/springpractice/service/EventService.java
+++ src/main/java/com/teddycrane/springpractice/service/EventService.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 import java.util.*;
 
 @Service
-class EventService implements IEventService
+public class EventService implements IEventService
 {
 
 	private final Logger logger;
