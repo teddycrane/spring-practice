d883d37114b8749f3cc077894df13816add8d5e1
Teddy Crane <teddy.crane@slalom.com>
2021-12-30 10:17:44 -0800

    temporarily lower test coverage threshold

--- src/main/java/com/teddycrane/springpractice/user/response/AuthenticationResponse.java
+++ src/main/java/com/teddycrane/springpractice/user/response/AuthenticationResponse.java
@@ -1,40 +1,42 @@
 package com.teddycrane.springpractice.user.response;
 
-public class AuthenticationResponse
-{
+public class AuthenticationResponse {
 
 	private boolean authenticated;
 	private String token;
 
-	public AuthenticationResponse()
-	{
+	public AuthenticationResponse() {
 		this.authenticated = true;
 		this.token = "";
 	}
 
-	public AuthenticationResponse(boolean authenticated, String token)
-	{
+	public AuthenticationResponse(boolean authenticated, String token) {
 		this.authenticated = authenticated;
 		this.token = token;
 	}
 
-	public boolean isAuthenticated()
-	{
+	public boolean isAuthenticated() {
 		return authenticated;
 	}
 
-	public void setAuthenticated(boolean authenticated)
-	{
+	public void setAuthenticated(boolean authenticated) {
 		this.authenticated = authenticated;
 	}
 
-	public String getToken()
-	{
+	public String getToken() {
 		return this.token;
 	}
 
-	public void setToken(String token)
-	{
+	public void setToken(String token) {
 		this.token = token;
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (other.getClass().equals(this.getClass())) {
+			AuthenticationResponse response = (AuthenticationResponse) other;
+			return response.isAuthenticated() == this.isAuthenticated() && response.getToken().equals(this.token);
+		}
+		return false;
 	}
 }
---
+++ src/test/java/com/teddycrane/springpractice/tests/controllertests/UserControllerTest.java
@@ -0,0 +1,115 @@
+package com.teddycrane.springpractice.tests.controllertests;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.UUID;
+
+import com.teddycrane.springpractice.enums.UserType;
+import com.teddycrane.springpractice.error.BadRequestException;
+import com.teddycrane.springpractice.user.User;
+import com.teddycrane.springpractice.user.UserController;
+import com.teddycrane.springpractice.user.model.IUserController;
+import com.teddycrane.springpractice.user.model.IUserService;
+import com.teddycrane.springpractice.user.request.AuthenticationRequest;
+import com.teddycrane.springpractice.user.response.AuthenticationResponse;
+
+import org.junit.jupiter.api.*;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class UserControllerTest {
+    private IUserController userController;
+    private User user;
+    private UUID testUUID;
+
+    @Mock
+    private IUserService userService;
+
+    @Captor
+    private ArgumentCaptor<UUID> uuidCaptor;
+
+    @Captor
+    private ArgumentCaptor<String> usernameCaptor, emailCaptor, passwordCaptor;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+
+        this.userController = new UserController(this.userService);
+        this.user = new User(UserType.USER, "first", "last", "username", "password", "email@email.fake");
+        this.testUUID = user.getId();
+    }
+
+    @Test
+    public void shouldGetRace() {
+        when(this.userService.getUser(testUUID)).thenReturn(user);
+
+        User result = this.userController.getUser(testUUID.toString());
+        verify(userService).getUser(uuidCaptor.capture());
+
+        Assertions.assertEquals(testUUID, uuidCaptor.getValue());
+        Assertions.assertTrue(result.equals(user));
+    }
+
+    @Test
+    public void shouldExceptWhenInvalidUUIDProvided() {
+        String invalidUUID = "test string";
+
+        Assertions.assertThrows(BadRequestException.class, () -> this.userController.getUser(invalidUUID));
+    }
+
+    @Test
+    public void shouldGetAllUsers() {
+        Collection<User> users = new ArrayList<>();
+        users.add(user);
+        when(this.userService.getAllUsers()).thenReturn(users);
+
+        Collection<User> result = this.userController.getAllUsers();
+
+        Assertions.assertTrue(result.equals(users));
+    }
+
+    @Test
+    public void shouldAuthenticateValidUsersWithUsername() {
+        AuthenticationResponse response = new AuthenticationResponse(true, "mock-token");
+        when(this.userService.login("username", null, "password")).thenReturn(response);
+
+        AuthenticationResponse result = this.userController
+                .login(new AuthenticationRequest("username", null, "password"));
+        verify(this.userService).login(usernameCaptor.capture(), emailCaptor.capture(), passwordCaptor.capture());
+
+        Assertions.assertEquals("username", usernameCaptor.getValue());
+        Assertions.assertEquals(null, emailCaptor.getValue());
+        Assertions.assertEquals("password", passwordCaptor.getValue());
+
+        Assertions.assertTrue(response.equals(result));
+    }
+
+    @Test
+    public void shouldAuthenticateWithEmail() {
+        AuthenticationResponse response = new AuthenticationResponse(true, "mock-token");
+        when(this.userService.login(null, "email", "password")).thenReturn(response);
+
+        AuthenticationResponse result = this.userController
+                .login(new AuthenticationRequest(null, "email", "password"));
+        verify(this.userService).login(usernameCaptor.capture(), emailCaptor.capture(), passwordCaptor.capture());
+
+        Assertions.assertEquals(null, usernameCaptor.getValue());
+        Assertions.assertEquals("email", emailCaptor.getValue());
+        Assertions.assertEquals("password", passwordCaptor.getValue());
+
+        Assertions.assertTrue(response.equals(result));
+    }
+
+    @Test
+    public void shouldNotAuthenticateWithoutEmailOrUsername() {
+        Assertions.assertThrows(BadRequestException.class,
+                () -> this.userController.login(new AuthenticationRequest(null, null, "password")));
+    }
+}
