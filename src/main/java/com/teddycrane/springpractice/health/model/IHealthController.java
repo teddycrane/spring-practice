eedf2a8c10c238419f761a4f42e01696f65dd904
Teddy Crane <78568061+teddycrane@users.noreply.github.com>
2022-01-05 14:17:17 -0800

    Add OpenAPI and Secret Keys in Env Variables (#23)
    * add auth ignore to api specs, add auth dep to pom
    
    * Get openapi docs displaying
    
    * Update docker compose and secret keys
    
    * bump coverage on JwtHelper
    
    * update pom and readme for integration testing setup
    
    * fix typo
    
    * Update circle to get automation running
    
    * add automation to circle config
    
    * add integration test
    
    * maybe fix circle automation step
    
    * add openjdk install orb
    
    * test push
    
    * test conditional job and changes
    
    * fix java version and circle config
    
    * fix typo
    
    * update config to fix pipeline build
    
    * fix yaml formatting
    
    * explicit jdk install in build machine
    
    * fix
    
    * run apt-get update
    
    * echo java home var for debug
    
    * fix
    
    * fix 2
    
    * fix from circle forums
    
    * re-add other steps

--- src/main/java/com/teddycrane/springpractice/health/model/IHealthController.java
+++ src/main/java/com/teddycrane/springpractice/health/model/IHealthController.java
@@ -5,11 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 @RestController
 @RequestMapping(path = "/health")
-public interface IHealthController
-{
+public interface IHealthController {
+	@Operation(summary = "Get the health status of the server.")
+	@ApiResponse(responseCode = "200", description = "Server status is healthy")
 	@GetMapping
 	Health getHealth();
 }
-
---
+++ src/test/java/com/teddycrane/springpractice/tests/integration/ITHealthController.java
@@ -0,0 +1,20 @@
+package com.teddycrane.springpractice.tests.integration;
+
+import com.teddycrane.springpractice.health.HealthController;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest(classes = { HealthController.class })
+public class ITHealthController {
+    @Autowired
+    private HealthController healthController;
+
+    @Test
+    public void contextLoads() {
+        Assertions.assertNotNull(healthController);
+    }
+}
