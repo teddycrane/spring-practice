be8c574ad7b19c87213903dfd6d23e5186c3fe81
Teddy Crane <teddy.crane@slalom.com>
2022-01-05 12:46:52 -0800

    add integration test

--- src/test/java/com/teddycrane/springpractice/tests/integration/ITHealthController.java
+++ src/test/java/com/teddycrane/springpractice/tests/integration/ITHealthController.java
@@ -5,7 +5,6 @@ import com.teddycrane.springpractice.health.HealthController;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
